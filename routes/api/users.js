7a4f6e8f810ff2d950911da01e6e1eb23049e2fa
Derek Wolpert <dfnj123@gmail.com>
2020-02-19 16:01:29 -0500

    user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require('../../config/keys');
 const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
@@ -27,6 +29,39 @@ router.post("/register", (req, res) => {
                 });
             });
         }
+    });
+});
+
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+    .then(user => {
+        if (!user) {
+            return res.status(404).json({ email: "This user does not exist" });
+        }
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if (isMatch) {
+
+                const payload = { id: user.id, name:user.name }
+                
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json({ password: "Incorrect password" });
+            }
+        });
     });
 });
 
