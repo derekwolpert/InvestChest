af00fb3a63988d71518a1ecf6573d268922520ce
Derek Wolpert <dfnj123@gmail.com>
2020-02-19 15:33:32 -0500

    implimented user registration route

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const trades = require("./routes/api/trades");
 
-const User = require("./models/User");
-
 mongoose
 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("Connected to MongoDB successfully"))
 	.catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
-
-app.use("/api/users", users);
-app.use("/api/trades", trades);
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+app.use("/api/users", users);
+app.use("/api/trades", trades);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
