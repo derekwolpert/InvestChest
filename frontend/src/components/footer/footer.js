e76303978e5752ce9274444f6153a5bb12e52b5c
Derek Wolpert <me@derekwolpert.com>
2020-02-26 16:44:25 -0500

    removed import of unused FontAwesomeIcon in footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedinIn, faAngellist, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedinIn, faAngellist } from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends React.Component {
     render() {
