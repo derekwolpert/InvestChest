9b348830cd7c52f67449b102e1347c615478ed80
Derek Wolpert <me@derekwolpert.com>
2020-03-06 19:05:39 -0500

    ensure sandbox api token is used in development

--- routes/api/stocks.js
+++ routes/api/stocks.js
@@ -6,14 +6,14 @@ const axios = require("axios");
 const iexApiToken = require("../../config/keys").iexApiToken;
 
 router.get("/batch/:symbols", (req, res) => {
-    axios.get(`https://cloud.iexapis.com/stable/stock/market/batch?symbols=${req.params.symbols}&filter=symbol,companyName,latestPrice,latestUpdate&types=quote&token=${iexApiToken}`)
+    axios.get(`https://${process.env.NODE_ENV === "production" ? "cloud" : "sandbox"}.iexapis.com/stable/stock/market/batch?symbols=${req.params.symbols}&filter=symbol,companyName,latestPrice,latestUpdate&types=quote&token=${iexApiToken}`)
         .then(stocks => res.json(stocks.data))
         .catch(err => res.status(err.response.status).json({ noStocksFound: err.response.data })
         );
 });
 
 router.get("/lookup/:symbol", (req, res) => {
-    axios.get(`https://cloud.iexapis.com/stable/stock/${req.params.symbol}/quote?filter=symbol,companyName,latestPrice,latestUpdate&token=${iexApiToken}`)
+    axios.get(`https://${process.env.NODE_ENV === "production" ? "cloud" : "sandbox"}.iexapis.com/stable/stock/${req.params.symbol}/quote?filter=symbol,companyName,latestPrice,latestUpdate&token=${iexApiToken}`)
         .then(stock => res.json(stock.data))
         .catch(err => res.status(err.response.status).json({ noStockFound: err.response.data, symbol: req.params.symbol.toUpperCase() }));
 });
